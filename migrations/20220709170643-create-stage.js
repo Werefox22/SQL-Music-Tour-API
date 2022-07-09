08cc09418cce25b1fbfef9fd6c2831e41579b323
Werefox <jack@wrightus.com>
2022-07-09 13:30:40 -0400

    Cleaned up the stage model and migration

--- migrations/20220709170643-create-stage.js
+++ migrations/20220709170643-create-stage.js
@@ -2,25 +2,15 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('stages', {
-      id: {
+      stage_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      stage_id: {
-        type: Sequelize.INTEGER
-      },
       stage_name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.STRING,
+        allowNull: false
       }
     });
   },
