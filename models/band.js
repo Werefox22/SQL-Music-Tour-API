2c0e8e8dd253b1ac0c13524e4b8ac91eb867e0b8
Werefox <jack@wrightus.com>
2022-07-09 11:23:22 -0400

    Initialized the Band model

--- models/band.js
+++ models/band.js
@@ -3,4 +3,33 @@ const sequelize = new Sequelize(process.env.PG_URI)
 
 class Band extends Model{}
 
+Band.init({
+	band_id: {
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	genre: {
+		type: DataTypes.TEXT,
+		allowNull: false
+	},
+	available_start_time: {
+		type: DataTypes.DATE,
+		allowNull: false
+	},
+	end_time: {
+		type: DataTypes.DATE,
+		allowNull: false
+	}
+}, {
+	sequelize,
+	modelName: 'Band',
+	tableName: 'band',
+	timestamps: false
+})
+
 module.exports = Band
