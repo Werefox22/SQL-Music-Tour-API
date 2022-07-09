5d9f5e8633bb70422e96a314427d4bf5bd5b8126
Werefox <jack@wrightus.com>
2022-07-09 12:43:26 -0400

    Cleaned up redundant code

--- models/band.js
+++ models/band.js
@@ -1,5 +1,4 @@
 const { Sequelize, DataTypes, Model } = require('sequelize')
-const sequelize = new Sequelize(process.env.PG_URI)
 
 class Band extends Model{}
 
--- server.js
+++ server.js
@@ -8,21 +8,6 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const sequelize = new Sequelize({
-    storage: process.env.PG_URI,
-    dialect: 'postgres',
-    username: 'postgres',
-    password: 'password'
-})
-
-try {
-    sequelize.authenticate();
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
-}
-catch {
-    console.log(`Failed to connect with Sequelize at ${process.env.PG_URI}`)
-}
-
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
