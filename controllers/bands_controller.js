51ffb664e91807fc4035dd1054133b802727a8f3
Werefox <jack@wrightus.com>
2022-07-11 19:49:49 -0400

    Updated the index route to allow searching by name

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -2,10 +2,17 @@ const bands = require('express').Router()
 const db = require('../models')
 const { Band } = db
 
+const { Op } = require('sequelize')
+
 // GET all bands
 bands.get('/', async (req, res) => {
 	try {
-		let foundBands = await Band.findAll()
+		let foundBands = await Band.findAll({
+			order: [ ['available_start_time', 'ASC']],
+			where: {
+				name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+			}
+		})
 		res.status(200).json(foundBands)
 	} catch (error) {
 		res.status(500).json(error)
