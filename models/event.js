219bbd8401230772267d0a6e18839f8c8f8d6167
Werefox <jack@wrightus.com>
2022-07-13 18:39:26 -0400

    Linked the stage and event models together with a many-to-many relationship

--- models/event.js
+++ models/event.js
@@ -4,13 +4,12 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate({ Stage, Stage_Event }) {
+      Event.belongsToMany(Stage, {
+        foreignKey: "event_id",
+        as: "stages",
+        through: Stage_Event
+      })
     }
   }
   Event.init({
